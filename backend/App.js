6b67e23cf457840f3141b2a4f9515a55d9f78e48
Harsh Patel <hahp1908@gmail.com>
2022-09-05 15:18:11 +0530

    Add DB Connected.

--- backend/App.js
+++ backend/App.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+const PORT = process.env.PORT || 3500;
+
+//connect to MongoDB
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Basic start");
 });
 
-const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+mongoose.connection.once('open',()=>{
+    console.log('Connected to MongoDB.');
+    app.listen(PORT,()=>{
+        console.log(`server running on port ${PORT} checkout: http://localhost:${PORT}`);
+    });
+});
